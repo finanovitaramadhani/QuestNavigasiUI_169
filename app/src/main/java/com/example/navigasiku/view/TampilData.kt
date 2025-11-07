fa9b512f569197f8083d3ca225a4dc9aae48e159
finanovita <finanovita479@gmail.com>
2025-11-07 12:32:11 +0700

    Menambahkan fungsi Composable TampilData yang menerima callback onBackBtnClick untuk navigasi kembali

---
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -0,0 +1,9 @@
+package com.example.navigasiku.view
+
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::Class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()-> Unit
+)
