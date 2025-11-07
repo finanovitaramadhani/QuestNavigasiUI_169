c1da7715ea938ba1eccb433a0ccca8c696798847
finanovita <finanovita479@gmail.com>
2025-11-07 14:03:37 +0700

    Menambahkan enum Navigasi sebagai penanda rute tampilan seperti Formulirku dan Detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
