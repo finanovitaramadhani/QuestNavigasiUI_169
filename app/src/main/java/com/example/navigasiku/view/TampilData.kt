d80510dcc65a226f1118a83e3f85d3d8c27659c8
finanovita <finanovita479@gmail.com>
2025-11-07 12:38:25 +0700

    Menambahkan list item berisi pasangan label dan nilai contoh untuk menampilkan data pengguna

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,9 +1,17 @@
 package com.example.navigasiku.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.navigasiku.R
 
 @OptIn(ExperimentalMaterial3Api::Class)
 @Composable
 fun TampilData(
     onBackBtnClick:()-> Unit
-)
+){
+    val item = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(id = R.string.alamat),"Yogyakarta")
+    )
+}
