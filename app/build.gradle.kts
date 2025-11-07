d502902a09ad4b3caa06faa9531e1637602e1a5f
finanovita <finanovita479@gmail.com>
2025-11-07 13:42:48 +0700

    Perbaikan file tampildata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.ui.text)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,42 +1,43 @@
 package com.example.navigasiku.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigasiku.R
 
-@OptIn(ExperimentalMaterial3Api::Class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    onBackBtnClick:()-> Unit
+    onBackBtnClick: ()->Unit
 ){
-    val item = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
-        Pair(stringResource(id = R.string.alamat),"Yogyakarta")
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
     Scaffold (modifier = Modifier,
-        topBar={
+        {
             TopAppBar(
-                title = { Text(stringResource(id=R.string.tampil), color = Color.white) },
+                title = {Text(text = stringResource(id=R.string.tampil), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
             )
         }) { isiRuang ->
@@ -46,7 +47,7 @@ fun TampilData(
         ) {
             Column(
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = 10 dp))
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id =  R.dimen.padding_small))
             ) {
                 items.forEach { item ->
                     Column {
@@ -67,7 +68,6 @@ fun TampilData(
                 ) {
                     Text(text = stringResource(id = R.string.back))
                 }
-
             }
         }
     }
